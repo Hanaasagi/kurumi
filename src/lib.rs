95601aa91f7e614d56c492b63ae18dd7e77a175a
Hanaasagi <ambiguous404@gmail.com>
2018-01-28 15:44:52 +0900

    add banner

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-#![feature(lang_items)]
 #![no_std]
+#![feature(asm)]
+#![feature(lang_items)]
 
 #[lang = "eh_personality"]
 #[no_mangle]
@@ -19,11 +20,21 @@ extern crate vga;
 extern crate device;
 use device::pic;
 
+extern crate interrupts;
+
 #[no_mangle]
 pub extern fn kmain() -> ! {
-    pic::remap();
     vga::clear_screen();
-    kprintln!("Welcome to Japari Park");
-    kprintln!("{}", "test format");
+    kprintln!("booting ...");
+    pic::remap();                   kprintln!("PIC INIT        {:>64}", "[ok]");
+    interrupts::init();             kprintln!("INTERRUPT INIT  {:>64}", "[ok]");
+    kprintln!(r"
+| | ___   _ _ __ _   _ _ __ ___ (_)
+| |/ | | | | '__| | | | '_ ` _ \| |
+|   <| |_| | |  | |_| | | | | | | |
+|_|\_\\__,_|_|   \__,_|_| |_| |_|_|
+    ");
+
+    kprint!("$ ");
     loop {}
 }
