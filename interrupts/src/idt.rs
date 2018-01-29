078c9b9f36109f1c31bb1ab65a658e45a0b32da7
Hanaasagi <ambiguous404@gmail.com>
2018-01-29 10:16:43 +0900

    Now support keyboard

--- interrupts/src/idt.rs
+++ interrupts/src/idt.rs
@@ -18,7 +18,7 @@ pub struct IdtEntry {
     offsetl:  u16,   // offset bits 0..15
     selector: u16,   // a code segment selector in GDT or LDT
     ist:      u8,    // bits 0..2 holds Interrupt Stack Table offset, rest of bits zero.
-    flags:    u8, // type and attributes
+    flags:    u8,    // type and attributes
     offsetm:  u16,   // offset bits 16..31
     offseth:  u32,   // offset bits 32..63
     zero:     u32,   // reserved
@@ -27,40 +27,31 @@ pub struct IdtEntry {
 
 impl IdtEntry {
 
-    // A "missing" IdtEntry.
-    //
-    // If the CPU tries to invoke a missing interrupt, it will instead
-    // send a General Protection fault (13), with the interrupt number and
-    // some other data stored in the error code.
-    pub const MISSING: IdtEntry = IdtEntry {
-        offsetl:  0,
-        selector: 0,
-        ist:      0,
-        flags:    0,
-        offsetm:  0,
-        offseth:  0,
-        zero:     0,
-    };
-
-    // Create a new IdtEntry pointing at `handler`, which must be a function
-    // with interrupt calling conventions.  (This must be currently defined in
-    // assembly language.)  The `gdt_code_selector` value must be the offset of
-    // code segment entry in the GDT.
-    //
-    // The "Present" flag set, which is the most common case.  If you need
-    // something else, you can construct it manually.
-    pub fn new(handler: usize, gdt_code_selector: u16) -> IdtEntry {
+    pub const fn new() -> IdtEntry {
         IdtEntry {
-            selector: gdt_code_selector,
-            offsetl:  handler as u16,
-            offsetm:  (handler >> 16) as u16,
-            offseth:  (handler >> 32) as u32,
+            offsetl:   0,
+            selector:  0,
             ist:      0,
-            // Nice bitflags operations don't work in const fn, hence these
-            // ad-hoc methods.
-            //
-            flags: (IdtFlags::PRESENT | IdtFlags::RING_0 | IdtFlags::INTERRUPT).bits,
-            zero:     0,
+            flags: 0,
+            offsetm:   0,
+            offseth:   0,
+            zero:     0
         }
+    }
+
+    pub fn set_flags(&mut self, flags: IdtFlags) {
+        self.flags = flags.bits;
+    }
+
+    pub fn set_offset(&mut self, selector: u16, base: usize) {
+        self.selector = selector;
+        self.offsetl = base as u16;
+        self.offsetm = (base >> 16) as u16;
+        self.offseth = (base >> 32) as u32;
+    }
+
+    pub fn set_func(&mut self, func: usize) {
+        self.set_flags(IdtFlags::PRESENT | IdtFlags::RING_0 | IdtFlags::INTERRUPT);
+        self.set_offset(0x08, func as usize);
     }
 }
---
+++ interrupts/src/lib.rs
@@ -0,0 +1,158 @@
+#![no_std]
+#![feature(asm)]
+#![feature(naked_functions)]
+#![feature(const_fn)]
+#![feature(core_intrinsics)]
+
+extern crate spin;
+#[macro_use]
+extern crate bitflags;
+
+mod dtables;
+pub mod idt;
+
+use spin::Mutex;
+use dtables::DescriptorTablePointer;
+use idt::IdtEntry;
+
+extern crate device;
+use device::pic;
+use device::keyboard;
+use device::io::inb;
+
+use core::intrinsics;
+
+#[macro_use]
+extern crate vga;
+
+macro_rules! scratch_push {
+    () => (asm!(
+        "push rax
+        push rcx
+        push rdx
+        push rdi
+        push rsi
+        push r8
+        push r9
+        push r10
+        push r11"
+        : : : : "intel", "volatile"
+    ));
+}
+
+macro_rules! scratch_pop {
+    () => (asm!(
+        "pop r11
+        pop r10
+        pop r9
+        pop r8
+        pop rsi
+        pop rdi
+        pop rdx
+        pop rcx
+        pop rax"
+        : : : : "intel", "volatile"
+    ));
+}
+
+macro_rules! preserved_push {
+    () => (asm!(
+        "push rbx
+        push rbp
+        push r12
+        push r13
+        push r14
+        push r15"
+        : : : : "intel", "volatile"
+    ));
+}
+
+macro_rules! preserved_pop {
+    () => (asm!(
+        "pop r15
+        pop r14
+        pop r13
+        pop r12
+        pop rbp
+        pop rbx"
+        : : : : "intel", "volatile"
+    ));
+}
+
+
+macro_rules! iret {
+    () => (asm!(
+        "iretq"
+        : : : : "intel", "volatile"
+    ));
+}
+
+#[macro_export]
+macro_rules! make_idt_entry {
+    ($index:expr, $name:ident, $body:expr) => {{
+        fn body() {
+            $body
+        }
+
+        #[naked]
+        unsafe extern fn $name() {
+            scratch_push!();
+            preserved_push!();
+            asm!("mov rsi, rsp
+                  push rsi
+
+                  cli
+
+                  call $0
+
+                  sti
+
+                  add rsp, 8" :: "s"(body as fn()) :: "volatile", "intel");
+            preserved_pop!();
+            scratch_pop!();
+            iret!();
+
+            intrinsics::unreachable();
+        }
+
+        IDT.lock()[$index].set_func($name as usize);
+    }};
+}
+
+// The Interrupt Descriptor Table
+// The CPU will look at this table to find the appropriate interrupt handler.
+pub  static IDT: Mutex<[IdtEntry; 256]> = Mutex::new([IdtEntry::new(); 256]);
+
+// Enable Interrupts.
+pub unsafe fn enable() {
+    asm!("sti");
+}
+
+// Disable Interrupts.
+pub unsafe fn disable() {
+    asm!("cli");
+}
+
+pub fn init() {
+
+    let ptr: DescriptorTablePointer =
+        DescriptorTablePointer::new_idtp(&IDT.lock()[..]);
+
+    unsafe { dtables::lidt(&ptr) };
+
+    make_idt_entry!(32, isr32, {
+        pic::send_eoi(32);
+    });
+
+    make_idt_entry!(33, isr33, {
+        let scancode = unsafe { inb(0x60) };
+
+        if let Some(c) = keyboard::from_scancode(scancode as usize) {
+            kprint!("{}", c);
+        }
+
+        pic::send_eoi(33);
+    });
+
+    unsafe { enable(); }
+}
