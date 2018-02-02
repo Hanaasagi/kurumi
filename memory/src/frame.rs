b2d008705e2245c1fdac08cc1bae9c6c19cfc839
Hanaasagi <ambiguous404@gmail.com>
2018-02-02 20:42:53 +0900

    add page entry

--- memory/src/frame.rs
+++ memory/src/frame.rs
@@ -1,4 +1,6 @@
-const PAGE_SIZE: usize = 4096;
+use super::PhysicalAddress;
+
+pub const PAGE_SIZE: usize = 4096;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Frame {
@@ -8,6 +10,10 @@ pub struct Frame {
 impl Frame {
     pub fn containing_address(address: usize) -> Frame {
         Frame{ number: address / PAGE_SIZE }
+    }
+
+    pub fn start_address(&self) -> PhysicalAddress {
+        self.number * PAGE_SIZE
     }
 }
 
--- memory/src/lib.rs
+++ memory/src/lib.rs
@@ -1,9 +1,17 @@
 // some code was from http://os.phil-opp.com/allocating-frames.html
 #![no_std]
+#![feature(const_fn)]
 
+#[macro_use]
+extern crate bitflags;
 extern crate multiboot2;
 
 pub mod frame;
 pub mod bump;
+pub mod paging;
 
 pub use bump::BumpAllocator;
+pub use frame::Frame;
+
+pub type PhysicalAddress = usize;
+pub type VirtualAddress  = usize;
