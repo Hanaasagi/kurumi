b80dc8f474ae58f298d9c989a7087be06e8679bc
Hanaasagi <ambiguous404@gmail.com>
2018-02-05 20:22:36 +0900

    align 4k

--- memory/src/paging/mapper.rs
+++ memory/src/paging/mapper.rs
@@ -1,5 +1,5 @@
 use super::{VirtualAddress, PhysicalAddress, Page};
-use super::entry::{ENTRY_COUNT, EntryFlags};
+use super::entry::*;
 use super::table::{self, Table, Level4};
 use super::{PAGE_SIZE, Frame, FrameAllocator};
 use core::ptr::Unique;
@@ -10,9 +10,7 @@ pub struct Mapper {
 
 impl Mapper {
     pub unsafe fn new() -> Mapper {
-        Mapper {
-            p4: Unique::new_unchecked(table::P4)
-        }
+        Mapper { p4: Unique::new_unchecked(table::P4) }
     }
 
     pub fn p4(&self) -> &Table<Level4> {
@@ -23,8 +21,7 @@ impl Mapper {
         unsafe { self.p4.as_mut() }
     }
 
-    pub fn translate(&self, virtual_address: VirtualAddress)
-        -> Option<PhysicalAddress> {
+    pub fn translate(&self, virtual_address: VirtualAddress) -> Option<PhysicalAddress> {
         let offset = virtual_address % PAGE_SIZE;
         self.translate_page(Page::containing_address(virtual_address))
             .map(|frame| frame.number * PAGE_SIZE + offset)
@@ -32,6 +29,7 @@ impl Mapper {
 
     pub fn translate_page(&self, page: Page) -> Option<Frame> {
         let p3 = self.p4().next_table(page.p4_index());
+
         p3.and_then(|p3| p3.next_table(page.p3_index()))
             .and_then(|p2| p2.next_table(page.p2_index()))
             .and_then(|p1| p1[page.p1_index()].pointed_frame())
@@ -39,18 +37,17 @@ impl Mapper {
 
     pub fn map_to<A>(&mut self, page: Page, frame: Frame,
                      flags: EntryFlags, allocator: &mut A) where A: FrameAllocator {
-        let p4 = self.p4_mut();
-        let mut p3 = p4.next_table_create(page.p4_index(), allocator);
-        let mut p2 = p3.next_table_create(page.p3_index(), allocator);
-        let mut p1 = p2.next_table_create(page.p2_index(), allocator);
+        let p3 = self.p4_mut().next_table_create(page.p4_index(), allocator);
+        let p2 = p3.next_table_create(page.p3_index(), allocator);
+        let p1 = p2.next_table_create(page.p2_index(), allocator);
 
         assert!(p1[page.p1_index()].is_unused());
-        p1[page.p1_index()].set(frame, flags | EntryFlags::PRESENT);
+        p1[page.p1_index()].set(frame, flags | PRESENT);
     }
 
     pub fn map<A>(&mut self, page: Page, flags: EntryFlags,
                   allocator: &mut A) where A: FrameAllocator {
-        let frame = allocator.alloc().expect("out of memory");
+        let frame = allocator.allocate_frame().expect("out of memory");
         self.map_to(page, frame, flags, allocator)
     }
 
@@ -60,19 +57,21 @@ impl Mapper {
         self.map_to(page, frame, flags, allocator)
     }
 
-    pub fn unmap<A>(&mut self, page: Page, allocator: &mut A) where A: FrameAllocator {
+    pub fn unmap<A>(&mut self, page: Page, _allocator: &mut A) where A: FrameAllocator {
+        use x86_64::VirtualAddress;
+        use x86_64::instructions::tlb;
+
         assert!(self.translate(page.start_address()).is_some());
+
         let p1 = self.p4_mut()
             .next_table_mut(page.p4_index())
             .and_then(|p3| p3.next_table_mut(page.p3_index()))
             .and_then(|p2| p2.next_table_mut(page.p2_index()))
             .expect("mapping code does not support huge pages");
-        let frame = p1[page.p1_index()].pointed_frame().unwrap();
+        //let frame = p1[page.p1_index()].pointed_frame().unwrap();
         p1[page.p1_index()].set_unused();
-        // clear TLB(translation lookaside buffer)
-        unsafe {
-            asm!("invlpg ($0)" :: "r" (page.start_address()) : "memory")
-        };
-        //allocator.free(frame);
+        tlb::flush(VirtualAddress(page.start_address()));
+        // TODO free p(1,2,3) table if empty
+        // allocator.deallocate_frame(frame);
     }
 }
