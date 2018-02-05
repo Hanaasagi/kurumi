34d7d23404111e86d28c42683bf38e061d5378c8
Hanaasagi <ambiguous404@gmail.com>
2018-02-05 20:16:32 +0900

    new entry

--- memory/src/paging/entry.rs
+++ memory/src/paging/entry.rs
@@ -1,12 +1,36 @@
-extern crate multiboot2;
-
 use super::Frame;
 use multiboot2::ElfSection;
 
-// a page table has 512 entry
-pub const ENTRY_COUNT: usize = 512;
-
 pub struct Entry(u64);
+
+impl Entry {
+    pub fn is_unused(&self) -> bool {
+        self.0 == 0
+    }
+
+    pub fn set_unused(&mut self) {
+        self.0 = 0;
+    }
+
+    pub fn flags(&self) -> EntryFlags {
+        EntryFlags::from_bits_truncate(self.0)
+    }
+
+    pub fn pointed_frame(&self) -> Option<Frame> {
+        if self.flags().contains(PRESENT) {
+            Some(Frame::containing_address(
+                self.0 as usize & 0x000fffff_fffff000,
+            ))
+        } else {
+            None
+        }
+    }
+
+    pub fn set(&mut self, frame: Frame, flags: EntryFlags) {
+        assert!(frame.start_address() & !0x000fffff_fffff000 == 0);
+        self.0 = (frame.start_address() as u64) | flags.bits();
+    }
+}
 
 bitflags! {
     pub struct EntryFlags: u64 {
@@ -23,50 +47,25 @@ bitflags! {
     }
 }
 
-impl Entry {
-    pub fn is_unused(&self) -> bool {
-        self.0 == 0
-    }
-
-    pub fn set_unused(&mut self) {
-        self.0 = 0;
-    }
-
-    pub fn flags(&self) -> EntryFlags {
-        EntryFlags::from_bits_truncate(self.0)
-    }
-
-    pub fn pointed_frame(&self) -> Option<Frame> {
-        if self.flags().contains(EntryFlags::PRESENT) {
-            // get 12-51 bit
-            let addr = (self.0 as usize) & 0x000fffff_fffff000;
-            Some(Frame::containing_address(addr))
-        } else {
-            None
-        }
-    }
-
-    pub fn set(&mut self, frame: Frame, flags: EntryFlags) {
-        assert!(frame.start_address() & !0x000fffff_fffff000 == 0);
-        self.0 = (frame.start_address() as u64) | flags.bits();
-    }
-}
-
 impl EntryFlags {
     pub fn from_elf_section_flags(section: &ElfSection) -> EntryFlags {
-        use multiboot2::{ELF_SECTION_ALLOCATED, ELF_SECTION_WRITABLE,
-            ELF_SECTION_EXECUTABLE};
+        use multiboot2::{
+            ELF_SECTION_ALLOCATED,
+            ELF_SECTION_WRITABLE,
+            ELF_SECTION_EXECUTABLE
+        };
+
         let mut flags = EntryFlags::empty();
 
         if section.flags().contains(ELF_SECTION_ALLOCATED) {
-            flags = flags | EntryFlags::PRESENT;
+            // section is loaded to memory
+            flags = flags | PRESENT;
         }
-
         if section.flags().contains(ELF_SECTION_WRITABLE) {
-            flags = flags | EntryFlags::WRITABLE;
+            flags = flags | WRITABLE;
         }
         if !section.flags().contains(ELF_SECTION_EXECUTABLE) {
-            flags = flags | EntryFlags::NO_EXECUTE;
+            flags = flags | NO_EXECUTE;
         }
 
         flags
