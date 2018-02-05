73d718b25ca4897d58dc3aec1ede3a555aaba92b
Hanaasagi <ambiguous404@gmail.com>
2018-02-05 13:59:37 +0900

    add temporary_page

--- memory/src/paging/mod.rs
+++ memory/src/paging/mod.rs
@@ -1,12 +1,19 @@
 use super::frame::{Frame, PAGE_SIZE};
 use super::frame::FrameAllocator;
 use super::{PhysicalAddress, VirtualAddress};
+use core::ops::{Add, Deref, DerefMut};
+use multiboot2::BootInformation;
 
 pub mod entry;
 pub mod table;
 pub mod mapper;
+pub mod temporary_page;
 
+use self::entry::EntryFlags;
+use self::temporary_page::TemporaryPage;
+use self::mapper::Mapper;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Page {
     number: usize,
 }
@@ -37,4 +44,194 @@ impl Page {
     fn p1_index(&self) -> usize {
         (self.number >> 0) & 0o777
     }
+
+    pub fn range_inclusive(start: Page, end: Page) -> PageIter {
+        PageIter {
+            start: start,
+            end: end,
+        }
+    }
+}
+
+impl Add<usize> for Page {
+    type Output = Page;
+
+    fn add(self, rhs: usize) -> Page {
+        Page { number: self.number + rhs }
+    }
+}
+
+#[derive(Clone)]
+pub struct PageIter {
+    start: Page,
+    end: Page,
+}
+
+impl Iterator for PageIter {
+    type Item = Page;
+
+    fn next(&mut self) -> Option<Page> {
+        if self.start <= self.end {
+            let page = self.start;
+            self.start.number += 1;
+            Some(page)
+        } else {
+            None
+        }
+    }
+}
+
+pub struct ActivePageTable {
+    mapper: Mapper,
+}
+
+impl Deref for ActivePageTable {
+    type Target = Mapper;
+
+    fn deref(&self) -> &Mapper {
+        &self.mapper
+    }
+}
+
+impl DerefMut for ActivePageTable {
+    fn deref_mut(&mut self) -> &mut Mapper {
+        &mut self.mapper
+    }
+}
+
+impl ActivePageTable {
+    unsafe fn new() -> ActivePageTable {
+        ActivePageTable {
+            mapper: Mapper::new()
+        }
+    }
+
+    pub fn with<F>(&mut self, table: &mut InactivePageTable,
+                   temporary_page: &mut temporary_page::TemporaryPage,
+                   f: F) where F: FnOnce(&mut Mapper) {
+        {
+            let cr3_val = cr3();
+            let backup = Frame::containing_address(cr3_val as usize);
+
+            // map temporary_page to current p4 table
+            let p4_table = temporary_page.map_table_frame(backup.clone(), self);
+
+            // overwrite recursive mapping
+            self.p4_mut()[511].set(table.p4_frame.clone(),
+                entry::EntryFlags::PRESENT | entry::EntryFlags::WRITABLE);
+            reload_cr3();
+
+            // execute f in the new context
+            f(self);
+
+            // restore recursive mapping to original p4 table
+            p4_table[511].set(backup,
+                entry::EntryFlags::PRESENT | entry::EntryFlags::WRITABLE);
+            reload_cr3();
+        }
+        temporary_page.unmap(self);
+    }
+
+    pub fn switch(&mut self, new_table: InactivePageTable) -> InactivePageTable {
+        let old_table = InactivePageTable {
+            p4_frame: Frame::containing_address(cr3() as usize),
+        };
+        unsafe { cr3_write(new_table.p4_frame.start_address() as u64); }
+        old_table
+    }
+}
+
+pub struct InactivePageTable {
+    p4_frame: Frame,
+}
+
+impl InactivePageTable {
+    pub fn new(frame: Frame, active_table: &mut ActivePageTable,
+               temporary_page: &mut TemporaryPage) -> InactivePageTable {
+        {
+            let table = temporary_page.map_table_frame(frame.clone(), active_table);
+            table.zero();
+            table[511].set(frame.clone(), EntryFlags::PRESENT | EntryFlags::WRITABLE);
+        }
+        temporary_page.unmap(active_table);
+
+        InactivePageTable { p4_frame: frame }
+    }
+}
+
+pub fn remap_the_kernel<A>(allocator: &mut A, boot_info: &BootInformation)
+    -> ActivePageTable where A: FrameAllocator {
+    let mut temporary_page = TemporaryPage::new(Page { number: 0xcafebabe }, allocator);
+
+    let mut active_table = unsafe { ActivePageTable::new() };
+    let mut new_table = {
+        let frame = allocator.alloc().expect("no more frames");
+        InactivePageTable::new(frame, &mut active_table, &mut temporary_page)
+    };
+
+    active_table.with(&mut new_table, &mut temporary_page, |mapper| {
+        let elf_sections_tag = boot_info
+            .elf_sections_tag()
+            .expect("Memory map tag required");
+
+        // identity map the allocated kernel sections
+        for section in elf_sections_tag.sections() {
+            if !section.is_allocated() {
+                // section is not loaded to memory
+                continue;
+            }
+
+            assert!(
+                section.addr as usize % PAGE_SIZE == 0,
+                "sections need to be page aligned"
+            );
+            kprintln!(
+                "mapping section at addr: {:#x}, size: {:#x}",
+                section.addr,
+                section.size
+            );
+
+            let flags = EntryFlags::from_elf_section_flags(section);
+
+            let start_frame = Frame::containing_address(section.start_address());
+            let end_frame = Frame::containing_address(section.end_address() - 1);
+            for frame in Frame::range_inclusive(start_frame, end_frame) {
+                mapper.identity_map(frame, flags, allocator);
+            }
+        }
+
+        // identity map the VGA text buffer
+        let vga_buffer_frame = Frame::containing_address(0xb8000);
+        mapper.identity_map(vga_buffer_frame, EntryFlags::WRITABLE, allocator);
+
+        // identity map the multiboot info structure
+        let multiboot_start = Frame::containing_address(boot_info.start_address());
+        let multiboot_end = Frame::containing_address(boot_info.end_address() - 1);
+        for frame in Frame::range_inclusive(multiboot_start, multiboot_end) {
+            mapper.identity_map(frame, EntryFlags::PRESENT, allocator);
+        }
+    });
+
+    let old_table = active_table.switch(new_table);
+    kprintln!("NEW TABLE!!!");
+
+    let old_p4_page = Page::containing_address(old_table.p4_frame.start_address());
+    active_table.unmap(old_p4_page, allocator);
+    kprintln!("guard page at {:#x}", old_p4_page.start_address());
+
+    active_table
+}
+
+fn cr3() -> u64 {
+    let ret: u64;
+    unsafe {asm!("mov %cr3, $0" : "=r" (ret));}
+    ret
+}
+
+unsafe fn cr3_write(val: u64) {
+    asm!("mov $0, %cr3" :: "r" (val) : "memory");
+}
+
+fn reload_cr3() {
+    unsafe { cr3_write(cr3()) }
 }
